ed856207aaa14e52d9d78988cdb178d29f9d63ee
Aazain Umrani <aazainumrani@gmail.com>
2024-12-20 01:12:25 +0500

    Initial Commit

---
+++ VideosMTests/VideosMTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideosMTests.swift
+//  VideosMTests
+//
+//  Created by Apple on 20/12/2024.
+//
+
+import Testing
+@testable import VideosM
+
+struct VideosMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
